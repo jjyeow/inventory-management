5432c9fca54dab1c81e86e35dab09cf4f2df58ef
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-01-17 15:15:42 +0800

    allow store to be added and uploaded into database

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import peeweedbevolve
 from flask import Flask, render_template, request
 from models import db
+from models import Store
 app = Flask(__name__)
 
 @app.before_request 
@@ -18,7 +19,14 @@ def migrate():
 
 @app.route("/")
 def index(): 
-    return render_template('index.html')
+    store_name = request.args.get('store_name')
+    store_details = Store(name=store_name)
+    store_details.save()
+    return render_template('index.html', store_name=store_name)
+
+@app.route("/shop")
+def shop(): 
+    return render_template('shop.html')
 
 if __name__ == '__main__':
     app.run() 

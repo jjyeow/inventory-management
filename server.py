bfb9ed49d4637a2b6404277002e7c0ed8d7e070b
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-01-17 16:14:00 +0800

    following Josh Teng method, allows flash to return message and implement redirect

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 import peeweedbevolve
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, url_for, redirect
 from models import db
 from models import Store
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 @app.before_request 
 def before_request(): 
     db.connect() 
@@ -19,14 +19,22 @@ def migrate():
 
 @app.route("/")
 def index(): 
-    store_name = request.args.get('store_name')
-    store_details = Store(name=store_name)
-    store_details.save()
-    return render_template('index.html', store_name=store_name)
+    return render_template('index.html')
 
 @app.route("/shop")
 def shop(): 
     return render_template('shop.html')
+
+@app.route("/shop_form")
+def shop_form(): 
+    store_name = request.args.get('store_name')
+    store_details = Store(name=store_name)
+    if store_details.save(): 
+        flash('Store sucessfully saved!')
+        return redirect(url_for('index'))
+    else: 
+        return render_template('shop.html', store_name = store_name)
+
 
 if __name__ == '__main__':
     app.run() 

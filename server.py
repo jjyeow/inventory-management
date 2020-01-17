175f559f161fa2dd8ef29cb21ba508a824345d08
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-01-17 18:49:59 +0800

    introduce warehouse input, also allows to choose store for each warehouse using dropdown input

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import peeweedbevolve
 from flask import Flask, render_template, request, flash, url_for, redirect
 from models import db
-from models import Store
+from models import Store, Warehouse
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 @app.before_request 
@@ -25,6 +25,25 @@ def index():
 def shop(): 
     return render_template('shop.html')
 
+@app.route("/warehouse")
+def warehouse():
+    stores = Store.select()
+    return render_template('warehouse.html', stores = stores)
+
+@app.route("/warehouse_form", methods = ["POST"])
+def warehouse_form():
+    store_id = request.form.get('store_id')
+    warehouse_location = request.form.get('location_name')
+    # breakpoint()
+    # store = Store.get_by_id(store_id) #if wanna pass Warehouse first argument as instance
+    new_warehouse = Warehouse(store_id = store_id, location=warehouse_location)
+    
+    if new_warehouse.save():
+        return redirect(url_for('warehouse'))
+    
+    else: 
+        return render_template('warehouse.html', store_id = store_id, warehouse_location = warehouse_location) 
+    
 @app.route("/shop_form", methods = ["POST"])
 def shop_form(): 
     store_name = request.form.get('store_name')
